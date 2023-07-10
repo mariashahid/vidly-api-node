3706548537535622ede00cd7d8c4665723d49d76
Maria Shahid <maria.shahid112@gmail.com>
2023-07-10 20:32:23 +0500

    Updated index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const app = express();
 require("./startup/logging")();
 require("./startup/cors")(app);
 require("./startup/routes")(app);
-require("./startup/db")();
 require("./startup/config")();
+require("./startup/db")();
 require("./startup/validation")();
 
 const port = process.env.PORT || config.get("port");
