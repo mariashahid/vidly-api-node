88d9ec15e7a4c065a9bb369490afbe5b0e261624
Maria Shahid <maria.shahid112@gmail.com>
2023-07-11 16:40:26 +0500

    Updated.

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const config = require("config");
 const app = express();
 
+const path = require("path");
+process.env["NODE_CONFIG_DIR"] = path.join(path.resolve("./"), "config/");
+
 require("./startup/logging")();
 require("./startup/cors")(app);
 require("./startup/routes")(app);
