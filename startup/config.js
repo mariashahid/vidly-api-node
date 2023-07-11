bf9acbafb2e45b13f235ecf24222a46ebaea04a1
Maria Shahid <maria.shahid112@gmail.com>
2023-07-11 16:55:58 +0500

    Updated.

--- startup/config.js
+++ startup/config.js
@@ -1,7 +1,6 @@
 const config = require("config");
 
 module.exports = function () {
-  console.error("config", config);
   if (!config.has("jwtPrivateKey")) {
     throw new Error("FATAL ERROR: jwtPrivateKey is not defined.");
   }
