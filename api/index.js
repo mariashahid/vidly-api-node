01962108b0a470e8155d0436eda777888e304e20
Maria Shahid <maria.shahid112@gmail.com>
2023-08-08 17:01:20 +0500

    Updated Index.js.

--- api/index.js
+++ api/index.js
@@ -3,6 +3,30 @@ const express = require("express");
 const config = require("config");
 const app = express();
 
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 const path = require("path");
 process.env["NODE_CONFIG_DIR"] = path.join(path.resolve("./"), "config/");
 
@@ -14,8 +38,8 @@ require("../startup/db")();
 require("../startup/validation")();
 
 const port = process.env.PORT || config.get("port");
-// const server = app.listen(port, () =>
-//   winston.info(`Listening on port ${port}...`)
-// );
+const server = app.listen(port, () =>
+  winston.info(`Listening on port ${port}...`)
+);
 
 module.exports = app;
